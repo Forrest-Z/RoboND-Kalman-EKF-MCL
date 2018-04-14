35d035d957cb9a446d5fabcdb0b0228daca7358b
Muthanna A. Attyah <mkhuthir@gmail.com>
2018-04-14 11:51:04 +0400

    prog: modified probability.cpp

--- probability.cpp
+++ probability.cpp
@@ -5,14 +5,20 @@ using namespace std;
 
 double f(double mu, double sigma2, double x)
 {
-    //Use mu, sigma2 (sigma squared), and x to code the 1-dimensional Gaussian
-    //Put your code here
+// 1-dimensional Gaussian
     double prob = 1.0 / sqrt(2.0 * M_PI * sigma2) * exp(-0.5 * pow((x - mu), 2.0) / sigma2);
     return prob;
 }
 
 int main()
 {
-    cout << f(10.0, 4.0, 8.0) << endl;
+    double mu = 10.0;       // mean value
+    double sigma2 = 4.0;    // sigma sequared
+    double x = 8.0;         // x
+
+    cout << "Mean = " << mu << endl;
+    cout << "Sigma Sequared = " << sigma2 << endl;
+    cout << "x = " << x << endl;
+    cout << "Probability = " << f(mu, sigma2, x) << endl << endl;
     return 0;
 }
